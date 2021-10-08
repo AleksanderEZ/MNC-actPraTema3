f01c148b87ab93ebed7061060f5151d646625e19
Entrar <Entrar@PC-1442>
2021-10-08 12:31:38 +0100

    hola buenas

--- main.cpp
+++ main.cpp
@@ -4,12 +4,17 @@
 #include <mkl.h>
 
 void ejercicio1();
-void ejercicio2();
+void ejercicio2apartado1();
+void ejercicio2apartado2();
+void showArray(double* array, int arraySize);
+void showArrayText(double* array, int arraySize);
+double map(double original);
 
 int main() {
 	
 	ejercicio1();
-	ejercicio2();
+	ejercicio2apartado1();
+	ejercicio2apartado2();
 
 
 	return 0;
@@ -27,9 +32,38 @@ void ejercicio1() {
 	printf("Tiempo: %lf usec\n", (fin - inicio) * 1.0e6);
 }
 
-void ejercicio2() {
-	double vector1[3] = { 5.0,27.0,1.0 };
+void ejercicio2apartado1() {
+	double vector1[3] = { 5.0,0.0,27.0 };
 	double vector2[3] = { 0.0,5.0,0.0 };
 
-	printf("Producto escalar: %lf", cblas_ddot(3, vector1, 1, vector2, 1));
+	printf("Producto escalar: %lf\n", cblas_ddot(3, vector1, 1, vector2, 1));
+}
+
+void ejercicio2apartado2() {
+	double vN[11] = { 'A', 'l','e','k','s','a','n','d','e','r' };
+	double vA[11] = {'O', 'l', 'i', 'v', 'a', 'r', 'e', 's', 0, 0};
+
+	cblas_daxpy(11, 3, vA, 1, vN, 1);
+	for (int i = 0; i < 11; i++) {
+		vN[i] = map(vN[i]);
+	}
+	showArrayText(vN, 11);
+}
+
+void showArray(double* array, int arraySize) {
+	for (int i = 0; i < arraySize; i++) {
+		printf("%lf ", array[i]);
+	}
+	printf("\n");
+}
+
+void showArrayText(double* array, int arraySize) {
+	for (int i = 0; i < arraySize; i++) {
+		printf("%c", (char)array[i]);
+	}
+	printf("\n");
+}
+
+double map(double original) {
+	return 97 + (122 - 97) * (original / 461);
 }
